886dd4f2ef5cd1abf347a886147f756f5df816a4
seroth77 <seroth77@gmail.com>
2021-07-25 13:26:00 -0400

    init

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+/* Add JavaScript code here! */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
